380d65246e915071f03f155fe018d0f263bcd202
Jakob Harlan <jakob.harlan@uni-weimar.de>
2014-08-07 19:37:41 +0200

    *fixed little error

--- filesystemloader.py
+++ filesystemloader.py
@@ -6,12 +6,11 @@ from folder import folder
 from file import file
 
 def walk(current):
-  print current
   for child in os.listdir(current.path):
     full_path = join(current.path, child)
     if isdir(full_path):
       new_folder = folder(full_path)
-      new_folder.children.append(walk(full_path))
+      new_folder.children.append(walk(new_folder))
     else:
       new_file = file(full_path)
 
