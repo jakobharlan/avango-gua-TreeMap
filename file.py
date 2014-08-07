e07c29585f183ae1a2068424f3ff5716cc86ec4c
Jakob Harlan <jakob.harlan@uni-weimar.de>
2014-08-08 01:05:14 +0200

    +size of files

--- file.py
+++ file.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 
 from entity import entity
+from os.path import getsize
 
 class file(entity):
 
 	def __init__(self, path):
 		entity.__init__(self, path)
+		self.size = getsize(path)
 
 	def print_structure(self, depth = 0):
 		for i in range(depth):
 			print "  ",
-		print self.path
+		print self.path + "   Size: " + str(self.size) 
