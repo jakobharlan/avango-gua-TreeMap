c9135b7c8ce637b40a459476ed2ad2e4294dcfcc
Jakob Harlan <jakob.harlan@uni-weimar.de>
2014-08-08 16:42:18 +0200

    +element color depents on folder/file

--- tm_element.py
+++ tm_element.py
@@ -10,20 +10,23 @@ class TM_Element():
 		self.input_entity = input_entity
 		self.children = []
 
+		material = ""
 		if self.input_entity.__class__ == folder:
 			for child in self.input_entity.children:
 				self.children.append(TM_Element(child))
+			material = "data/materials/Grey.gmd"
+		else:
+			material = "data/materials/Cyan.gmd"
 
 		loader = avango.gua.nodes.TriMeshLoader()
+
 		self.geometry = loader.create_geometry_from_file(
 			"cube",
 			"data/objects/cube.obj",
-			"data/materials/Grey.gmd",
-			avango.gua.LoaderFlags.DEFAULTS
+			material,
+			avango.gua.LoaderFlags.DEFAULTS,
 		)
 
-	def layout(self):
-		pass
 
 	def create_scenegraph_structure(self):
 		for child in self.children:
