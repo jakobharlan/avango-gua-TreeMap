7e6b8aa511c64ef4c38014c1d0ac1274f552919c
Kristof <stuff@kritten.org>
2014-09-06 15:46:13 +0200

    *improved controller 3D code, increased nearclip=>rescaled pickers

--- controller3D.py
+++ controller3D.py
@@ -26,7 +26,6 @@ class Controller3D(avango.script.Script):
 	fall_speed = 0.0007
 	ascend_speed = 0.0007
 	run_speed = 4
-	collision_value = 0.0005
 
 	def __init__(self):
 		self.super(Controller3D).__init__()
@@ -45,7 +44,7 @@ class Controller3D(avango.script.Script):
 		rotation = avango.gua.make_rot_mat(-self.rel_rot_x * self.horizontal_speed, 0.0, 1.0, 0.0) * \
 							 avango.gua.make_rot_mat(-self.rel_rot_y * self.vertical_speed, 1.0, 0.0, 0.0) #calc view rotation
 
-		self.viewing_direction = self.get_ray_direction(self.Picker.Ray.value, avango.gua.Vec3(0.0001, 0.0001, 5))
+		self.viewing_direction = self.get_ray_direction(self.Picker.Ray.value, avango.gua.Vec3(0.00001, 0.00001, 5))
 
 
 		if self.Keyboard.KeyW.value:
@@ -87,7 +86,6 @@ class Controller3D(avango.script.Script):
 			self.position_y = self.height + self.size
 
 
-
 		self.Position.value += avango.gua.Vec3(MovementX, 0, MovementZ)
 
 		position_x = self.Position.value.x 						
@@ -97,8 +95,6 @@ class Controller3D(avango.script.Script):
 
 		self.OutTransform.value = avango.gua.make_trans_mat(self.Position.value) * \
 															rotation
-
-
 
 
 	def setPosition(self):
@@ -139,29 +135,20 @@ class Controller3D(avango.script.Script):
 	def config_Down_Picker(self):
 		self.Down_Picker.Ray.value.Transform.value = avango.gua.make_trans_mat(self.Position.value.x, 5, self.Position.value.z) * \
 																									avango.gua.make_rot_mat(-90, 1.0, 0.0, 0.0) * \
-																									avango.gua.make_scale_mat(0.0005, 0.0005, 10)
+																									avango.gua.make_scale_mat(0.00001, 0.00001, 10)
 
 	def config_Move_Picker(self, MovementX, MovementZ):
 		if not MovementX == 0 or not MovementZ == 0:
 			moving_direction = avango.gua.Vec2(MovementX, MovementZ)
 			standard_direction = avango.gua.Vec2(0, -1)	
 
-			a = ( moving_direction.x * standard_direction.x )
-			b = ( moving_direction.y * standard_direction.y )
-
-			angle = (360)/(2 * math.pi) * math.acos( (a+b)/( moving_direction.normalize() * standard_direction.normalize() ) )
+			angle = (360)/(2 * math.pi) * math.acos( (moving_direction.dot(standard_direction))/( moving_direction.normalize() * standard_direction.normalize() ) )
 			if moving_direction.x > 0:
 				angle = 360 - angle
 
 			self.Move_Picker.Ray.value.Transform.value = avango.gua.make_trans_mat(self.Position.value.x, self.Position.value.y - self.size+0.0001, self.Position.value.z) * \
 																								avango.gua.make_rot_mat(int(angle), 0.0, 1.0, 0.0) * \
-																								avango.gua.make_scale_mat(0.0001, 0.0001, 5)	
+																								avango.gua.make_scale_mat(0.00001, 0.00001, 5)	
 
 	def get_angle(self):
 		pass
-			
-		# if len(self.Move_Picker.Results.value) > 0:
-			# print self.Move_Picker.Results.value[0].Distance.value
-			# if self.Move_Picker.Results.value[0].Distance.value < self.collision_value:
-				# MovementX = 0
-				# MovementZ = 0
