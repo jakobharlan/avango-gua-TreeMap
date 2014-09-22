f7789edeed83f01384ced622b1933f4b15dd351e
Kristof <stuff@kritten.org>
2014-09-22 17:22:08 +0200

    *outview fixed with transitions

--- controller.py
+++ controller.py
@@ -13,8 +13,10 @@ import avango.gua
 
 class Navigator(avango.script.Script):
 	modus = avango.SFInt()
+	last_modus = avango.SFInt()
 	Is_patrick_modus = avango.SFBool()
 	modus.value = 0
+	last_modus.value = 0
 	controller2D = Controller2D()
 	controller3D = Controller3D()
 	controllerOut = ControllerOut()
@@ -51,53 +53,82 @@ class Navigator(avango.script.Script):
 	@field_has_changed(modus)
 	def update_mode(self):
 		self.running_animation = True
-		# print "Position2D" + str(self.controller2D.Position)
-		# print "Position3D" + str(self.controller3D.Position)
 		if self.modus.value == 0:
 			self.controller3D.Mouse = None
+			self.controllerOut.Keyboard = None
 
 			self.controller2D.Position.x = self.controller3D.Position.x
 			self.controller2D.Position.z = self.controller3D.Position.z
-			start_position = self.controller3D.Position
-			end_position = self.controller2D.Position
-
-			start_rotation = self.controller3D.Rotation.get_rotate()
-			end_rotation = avango.gua.make_rot_mat(-90, 1, 0, 0).get_rotate()
 
 			self.OutTransform.disconnect_from(self.controller3D.OutTransform)
+			self.OutTransform.disconnect_from(self.controllerOut.OutTransform)
+			end_position = self.controller2D.Position
+			end_rotation = avango.gua.make_rot_mat(-90, 1, 0, 0).get_rotate()
 			animation = Animation()
-			animation.my_constructor(start_position, end_position, start_rotation, end_rotation, self.animation_ended, True, 1.0)
+			
+			if self.last_modus.value == 1:
+				start_position = self.controller3D.Position
+				start_rotation = self.controller3D.Rotation.get_rotate()
+				animation.my_constructor(start_position, end_position, start_rotation, end_rotation, self.animation_ended, "First", 2.0)
+			else:
+				start_position = self.controllerOut.Position
+				start_rotation = self.controllerOut.Rotation.get_rotate()
+				animation.my_constructor(start_position, end_position, start_rotation, end_rotation, self.animation_ended, "Always", 1.0)
+
 			self.OutTransform.connect_from(animation.OutTransform)
 		elif self.modus.value == 1:
+			self.controllerOut.Keyboard = None
 			self.controller3D.Position.x = self.controller2D.Position.x
 			self.controller3D.Position.z = self.controller2D.Position.z
-			start_position = self.controller2D.Position
-			end_position = self.controller3D.Position
 
+			start_position = self.controller2D.Position
 			start_rotation = avango.gua.make_rot_mat(-90, 1, 0, 0).get_rotate()
+
+			end_position = self.controller3D.Position
 			end_rotation = self.controller3D.Rotation.get_rotate()
 
 			self.controller3D.height = self.controller2D.zoom - self.Picker.Results.value[0].Distance.value * 5
 			self.OutTransform.disconnect_from(self.controller2D.OutTransform)
 			animation = Animation()
-			animation.my_constructor(start_position, end_position, start_rotation, end_rotation, self.animation_ended, False, 1.0)
+			animation.my_constructor(start_position, end_position, start_rotation, end_rotation, self.animation_ended, "End", 2.0)
 			self.OutTransform.connect_from(animation.OutTransform)
 		elif self.modus.value == 2:
 			self.controller2D.Keyboard = None
 			self.controller3D.Mouse = None
 			self.controller3D.Keyboard = None
 
+			self.controllerOut.Position = avango.gua.Vec3(0, 2, 2)
+			self.controllerOut.angle.value = 90
+			self.controllerOut.heightAngle.value = -45
 
-			self.OutTransform.disconnect_from(self.controller2D.OutTransform)
+			Rotation = avango.gua.make_rot_mat(-(self.controllerOut.angle.value -90), 0.0, 1.0, 0.0) * \
+								 avango.gua.make_rot_mat(self.controllerOut.heightAngle.value, 1.0, 0.0, 0.0) #calc view rotation
+
+		 	self.controllerOut.OutTransform.value = avango.gua.make_trans_mat(self.controllerOut.Position) * \
+																Rotation
+
 			self.OutTransform.disconnect_from(self.controller3D.OutTransform)
+			self.OutTransform.disconnect_from(self.controller2D.OutTransform)
 
-			self.OutTransform.connect_from(self.controllerOut.OutTransform)
-			self.controllerOut.setKeyboard(self.Keyboard)
-			self.running_animation = False
+			if self.last_modus.value == 0:
+				start_position = self.controller2D.Position
+				start_rotation = avango.gua.make_rot_mat(-90, 1, 0, 0).get_rotate()
+			else:
+				start_position = self.controller3D.Position
+				start_rotation = self.controller3D.Rotation.get_rotate()
+
+			end_position = self.controllerOut.Position
+			rot = avango.gua.make_rot_mat(0, 0.0, 1.0, 0.0) * avango.gua.make_rot_mat(-45, 1.0, 0.0, 0.0)
+			end_rotation = rot.get_rotate()
+
+			animation = Animation()
+			animation.my_constructor(start_position, end_position, start_rotation, end_rotation, self.animation_ended, "Always", 1.0)
+			self.OutTransform.connect_from(animation.OutTransform)
 
 	def evaluate(self):
 		if not self.running_animation:
 			if self.Keyboard.KeySTRG.value and not self.KeySTRG:
+				self.last_modus.value = self.modus.value
 				if self.modus.value == 0:
 					self.modus.value = 1
 					print "3D"
@@ -105,6 +136,7 @@ class Navigator(avango.script.Script):
 					self.modus.value = 0
 					print "2D"
 			elif self.Keyboard.KeyALT.value and not self.KeyALT:
+				self.last_modus.value = self.modus.value
 				if self.modus.value == 0 or self.modus.value == 1:
 					self.modus.value = 2
 					print "overview"
@@ -113,10 +145,6 @@ class Navigator(avango.script.Script):
 					print "2D"
 			self.KeySTRG = self.Keyboard.KeySTRG.value
 			self.KeyALT = self.Keyboard.KeyALT.value
-
-			# if self.Keyboard.KeyALT.value and not self.KeyALT:
-			# 	self.Is_patrick_modus.value = not self.Is_patrick_modus.value
-			# self.KeyALT = self.Keyboard.KeyALT.value
 
 	def animation_ended(self):
 		self.OutTransform.disconnect()
@@ -127,6 +155,9 @@ class Navigator(avango.script.Script):
 			self.OutTransform.connect_from(self.controller3D.OutTransform)
 			self.controller3D.setMouse(self.Mouse)
 			self.controller3D.setKeyboard(self.Keyboard)
+		elif self.modus.value == 2:
+			self.OutTransform.connect_from(self.controllerOut.OutTransform)
+			self.controllerOut.setKeyboard(self.Keyboard)
 		self.running_animation = False
 
 class Animation(avango.script.Script):
@@ -137,12 +168,12 @@ class Animation(avango.script.Script):
 		self.super(Animation).__init__()
 		self.always_evaluate(True)
 
-	def my_constructor(self, start_position, end_position, start_rotation, end_rotation, callback, rotation_first = True, animation_duration = 3):
+	def my_constructor(self, start_position, end_position, start_rotation, end_rotation, callback, rotation = "Always", animation_duration = 3):
 		self.start_position = start_position
 		self.end_position = end_position
 		self.start_rotation = start_rotation
 		self.end_rotation = end_rotation
-		self.rotation_first = rotation_first
+		self.rotation = rotation
 
 		self.animation_duration = animation_duration
 		self.start_time = time.time()
@@ -156,16 +187,18 @@ class Animation(avango.script.Script):
 		progress = self.easing_function(current_time-self.start_time, 0, progress ,	self.animation_duration)
 
 		if progress < 1.0:
-			if self.rotation_first:
+			if self.rotation == "First":
 				if progress > 0.5:
 					current_rotation = avango.gua.make_rot_mat(self.end_rotation)
 				else:
 					current_rotation = avango.gua.make_rot_mat(self.start_rotation.slerp_to(self.end_rotation, progress * 2))
-			else:
+			elif self.rotation == "End":
 				if progress < 0.5:
 					current_rotation = avango.gua.make_rot_mat(self.start_rotation)
 				else:
 					current_rotation = avango.gua.make_rot_mat(self.start_rotation.slerp_to(self.end_rotation, (progress - 0.5) * 2))
+			elif self.rotation == "Always":
+				current_rotation = avango.gua.make_rot_mat(self.start_rotation.slerp_to(self.end_rotation, progress))
 			current_position = self.start_position.lerp_to(self.end_position, progress)
 			self.OutTransform.value = avango.gua.make_trans_mat(current_position) * current_rotation
 		else:
--- controllerOut.py
+++ controllerOut.py
@@ -11,8 +11,8 @@ class ControllerOut(avango.script.Script):
 	Position = avango.gua.Vec3()
 	Speed = avango.SFFloat()
 	Keyboard = None
-	Picker = None
 	angle = avango.SFFloat()
+	heightAngle = avango.SFFloat()
 	distance = 2
 
 
@@ -21,62 +21,11 @@ class ControllerOut(avango.script.Script):
 		self.always_evaluate(True)
 		self.Position = avango.gua.Vec3(0, 2, self.distance)
 		self.angle.value = 90
-		# self.Speed.value = 0.01
-		# self.zoom = 3
-		# self.zoomspeed = 0.1
+		self.heightAngle.value = -45
 
 	def evaluate(self):
-		# distance = 100
-		# MovementX = 0
-		# MovementZ = 0
-
-		# if len(self.Picker.Results.value) > 0:
-		# 	distance = self.Picker.Results.value[0].Distance.value
-		# else:
-		# 	distance = None
-
-		# if self.Keyboard != None:
-		# 	if self.Keyboard.KeyW.value and self.Position.z > -0.48 and self.Position.x > -0.48 and self.Position.x < 0.48:
-		# 		MovementZ = -1 * self.Speed.value
-
-		# 	if self.Keyboard.KeyA.value and self.Position.x > -0.48 and self.Position.z > -0.48 and self.Position.z < 0.48:
-		# 		MovementX = -1 * self.Speed.value
-
-		# 	if self.Keyboard.KeyS.value and self.Position.z < 0.48 and self.Position.x > -0.48 and self.Position.x < 0.48:
-		# 		MovementZ += 1 * self.Speed.value
-
-		# 	if self.Keyboard.KeyD.value and self.Position.x < 0.48 and self.Position.z > -0.48 and self.Position.z < 0.48:
-		# 		MovementX += 1 * self.Speed.value
-
-		# 	if not distance == None:	#if the view is over the map
-		# 		if distance < 1:					#if the  view is in a close range to the map
-		# 			self.Position += avango.gua.Vec3(MovementX * distance, 0, MovementZ * distance)	#slow down the movement speed depending on the distance
-		# 		else:											
-		# 			self.Position += avango.gua.Vec3(MovementX , 0, MovementZ)	#set the position normally
-
-		# 	if self.Keyboard.KeyQ.value:	#zoom out
-		# 		if not distance == None and self.Position.z > -0.48 and self.Position.x > -0.48 and self.Position.x < 0.48 and self.Position.z < 0.48:									#allow to zoom out while picker finds something only
-		# 			if distance < 1:												#if picker is more close to the map then 1
-		# 				self.zoom += ( 1 * distance * self.zoomspeed )	#zoom out slower
-		# 			else:
-		# 				self.zoom += ( 1 * self.zoomspeed )
-
-		# 	if self.Keyboard.KeyE.value and self.Position.z > -0.48 and self.Position.x > -0.48 and self.Position.x < 0.48 and self.Position.z < 0.48:	#zoom in
-		# 		if distance == None:												#allow zoom if too far away
-		# 			self.zoom -= ( 1 * self.zoomspeed )
-		# 		elif distance > 0.04:												#stop zoom if to close
-		# 			self.zoom -= ( 1 * distance * self.zoomspeed )
-
-		# positionx = self.Position.x 						
-		# positionz = self.Position.z
-		# self.Position = avango.gua.Vec3(positionx, self.zoom, positionz)		#set the zoom level only
-
-		# if (	self.Position.x < 0.48 and
-		# 			self.Position.x > -0.48 and
-		# 			self.Position.z < 0.48 and
-		# 			self.Position.z > -0.48):
-	
 		positiony = self.Position.y
+		heightAngle = 0
 
 		if self.Keyboard != None:
 			if self.Keyboard.KeyA.value:
@@ -85,32 +34,47 @@ class ControllerOut(avango.script.Script):
 				self.angle.value -= 1
 			if self.Keyboard.KeyQ.value and self.distance > 0.3:
 				self.distance -= 0.01
+				# heightAngle += 0.15
 				positiony -= 0.01
-			if self.Keyboard.KeyE.value and self.distance > 0.3:
+			if self.Keyboard.KeyE.value:
 				self.distance += 0.01
+				# heightAngle -= 0.15
 				positiony += 0.01
+			# if self.Keyboard.KeyS.value:
+			# 	positiony -= 0.01
+				# heightAngle += 0.15
+			# if self.Keyboard.KeyW.value:
+			# 	positiony += 0.01
+				# heightAngle -= 0.15
 
+			# self.heightAngle.value += heightAngle
 
-		positionx = math.cos(( ( 2*math.pi )/360 ) * self.angle.value)
-		positionz = math.sin(( ( 2*math.pi )/360 ) * self.angle.value)
+			positionx = math.cos(( ( 2*math.pi )/360 ) * self.angle.value)
+			positionz = math.sin(( ( 2*math.pi )/360 ) * self.angle.value)
 
-		# print "x"+str(positionx * self.distance)
-		# print "z"+str(positionz * self.distance)
+			self.Position.x = positionx * self.distance
+			self.Position.y = positiony
+			self.Position.z = positionz * self.distance
 
-		self.Position.x = positionx * self.distance
-		self.Position.y = positiony
-		self.Position.z = positionz * self.distance
+			self.Rotation = avango.gua.make_rot_mat(-(self.angle.value -90), 0.0, 1.0, 0.0) * \
+								 avango.gua.make_rot_mat(self.heightAngle.value, 1.0, 0.0, 0.0) #calc view rotation
 
-		self.Rotation = avango.gua.make_rot_mat(-(self.angle.value -90), 0.0, 1.0, 0.0) * \
-							 avango.gua.make_rot_mat(-45, 1.0, 0.0, 0.0) #calc view rotation
+			# self.Rotation = self.get_rot_mat()
 
-		# self.Rotation = avango.gua.make_rot_mat(-90, 1.0, 0.0, 0.0)
+			self.OutTransform.value = avango.gua.make_trans_mat(self.Position) * \
+																self.Rotation
 
-		self.OutTransform.value = avango.gua.make_trans_mat(self.Position) * \
-															self.Rotation
+	def get_rot_mat(self):
+		vec1 = avango.gua.Vec3(0, 0, -1)
+		vec2 = avango.gua.Vec3(0 - self.Position.x, 0 - self.Position.y, 0 - self.Position.z)
+
+		vec1.normalize()
+		vec2.normalize()
+
+		angle = math.degrees(math.acos(vec1.dot(vec2)))
+		axis = vec1.cross(vec2)
+
+		return avango.gua.make_rot_mat(angle, axis) 
 
 	def setKeyboard(self, Keyboard):
-		self.Keyboard = Keyboard
-
-	def setPicker(self, Picker):
-		self.Picker = Picker		
+		self.Keyboard = Keyboard	
